ce0c4b0c448b60993376cf7d0c419ac0f7e29678
4UPanElektryk <maciek.ce3@gmail.com>
2022-02-04 15:43:08 +0100

    Fixed Opening logs

--- Maciek OS Core/Commands/Cmds/Logs.cs
+++ Maciek OS Core/Commands/Cmds/Logs.cs
@@ -2,6 +2,7 @@
 using System;
 using MOS_Log_Integration;
 using Maciek_OS_Core.Essentials;
+using System.Threading;
 using System.IO;
 
 namespace Maciek_OS_Core.Commands.Cmds
@@ -24,10 +25,25 @@ namespace Maciek_OS_Core.Commands.Cmds
 						{
 							Console.Clear();
 							Dual.LogWatermark();
-							Log.ClearLogs();
+							Console.ForegroundColor = ConsoleColor.Yellow;
+							Console.Write("Do you want to delete logs? Y | N >>");
+                            if (Console.ReadKey().Key == ConsoleKey.Y)
+                            {
+                                Console.WriteLine();
+								foreach (string item in Directory.GetFiles(AppDomain.CurrentDomain.BaseDirectory + Config.LogsPath))
+								{
+									Dual.Msg("Log Deleted: '" + item + "'", ConsoleColor.Red);
+								}
+								Log.ClearLogs();
+							}
+                            else
+                            {
+                                Console.WriteLine();
+								Dual.Msg("Deletion of logs has been canceled", ConsoleColor.Green);
+							}
 							Console.ReadKey();
-							Dual.Watermark();
 							Console.Clear();
+							Dual.Watermark();
 						}
 						else
 						{
@@ -42,61 +58,63 @@ namespace Maciek_OS_Core.Commands.Cmds
 							Console.Clear();
 							Dual.LogWatermark();
 							Console.WriteLine("File nr: ");
-							int path = int.Parse(Console.ReadLine());
-							Console.WriteLine("");
-							string p = AppDomain.CurrentDomain.BaseDirectory + Config.LogsPath + "LOG" + path.ToString() + ".log";
-							try
-							{
-								string[] _file = File.ReadAllLines(@p);
-								foreach (string _item in _file)
+							int path = 0;
+							bool no = int.TryParse(Console.ReadLine(), out path);
+                            Console.WriteLine("");
+                            if (true)
+                            {
+								string p = AppDomain.CurrentDomain.BaseDirectory + Config.LogsPath + "LOG" + path.ToString() + ".log";
+								try
 								{
-									string[] w = _item.Split('|');
-									Console.Write("[" + w[0] + "]");
-									bool nok = false;
-									switch (w[1])
+									string[] _file = File.ReadAllLines(@p);
+									foreach (string _item in _file)
 									{
-										case "{[NORMAL]}":
-											nok = false;
-											break;
-										case "{[INFO]}":
-											nok = false;
-											Console.ForegroundColor = ConsoleColor.Blue;
-											break;
-										case "{[WARRNING]}":
-											nok = false;
-											Console.ForegroundColor = ConsoleColor.Yellow;
-											break;
-										case "{[ERROR]}":
-											nok = false;
-											Console.ForegroundColor = ConsoleColor.DarkRed;
-											break;
-										case "{[CRITICAL_ERROR]}":
-											nok = true;
-											Console.ForegroundColor = ConsoleColor.Red;
-											break;
-										default:
-											break;
-									}
-									Console.WriteLine(w[1]);
-									if (!nok)
-									{
+										string[] w = _item.Split('|');
+										Console.Write("[" + w[0] + "]");
+										bool nok = false;
+										switch (w[1])
+										{
+											case "{[NORMAL]}":
+												nok = false;
+												break;
+											case "{[INFO]}":
+												nok = false;
+												Console.ForegroundColor = ConsoleColor.Blue;
+												break;
+											case "{[WARRNING]}":
+												nok = false;
+												Console.ForegroundColor = ConsoleColor.Yellow;
+												break;
+											case "{[ERROR]}":
+												nok = false;
+												Console.ForegroundColor = ConsoleColor.DarkRed;
+												break;
+											case "{[CRITICAL_ERROR]}":
+												nok = true;
+												Console.ForegroundColor = ConsoleColor.Red;
+												break;
+											default:
+												break;
+										}
+										Console.WriteLine(w[1]);
+										if (!nok)
+										{
+											Console.ForegroundColor = ConsoleColor.White;
+										}
+										Console.WriteLine("Name of action: \n" + w[2]);
+										Console.WriteLine("Action: \n" + w[3]);
+										Console.WriteLine("");
 										Console.ForegroundColor = ConsoleColor.White;
 									}
-									Console.WriteLine("Name of action: \n" + w[2]);
-									Console.WriteLine("Action: \n" + w[3]);
-									Console.WriteLine("");
-									Console.ForegroundColor = ConsoleColor.White;
-
+									Console.ReadKey();
+									Console.Clear();
+									Dual.Watermark();
 								}
-								Console.ReadKey();
-								Console.Clear();
-								Dual.Watermark();
+								catch
+								{
+									Dual.Msg("File Not Found", ConsoleColor.Red);
+								}
 							}
-							catch
-							{
-								Dual.Msg("File Not Found", ConsoleColor.Red);
-							}
-
 						}
 						else
 						{
