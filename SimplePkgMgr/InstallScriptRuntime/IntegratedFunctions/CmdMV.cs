39c8fe30d4f6d1f1d35f85a6d10ea4db280d2299
4UPanElektryk <80508394+4UPanElektryk@users.noreply.github.com>
2023-01-29 11:02:05 +0100

    fixes to ISL

---
+++ SimplePkgMgr/InstallScriptRuntime/IntegratedFunctions/CmdMV.cs
@@ -0,0 +1,32 @@
+﻿using SimpleLogs4Net;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace SimplePkgMgr.InstallScriptRuntime.IntegratedFunctions
+{
+    public class CmdMV : Cmd
+    {
+        public CmdMV(string name) : base(name) { }
+        public override bool Execute(string[] args)
+        {
+            Log.Write("Moving " + args[0] + " to " + args[1]);
+            try
+            {
+                File.Copy(args[0], args[1]);
+                File.Delete(args[0]);
+            }
+            catch
+            {
+                Log.Write("Moving Failed " + args[0] + " to " + args[1], EType.Error);
+                return false;
+            }
+            Log.Write("Moved " + args[0] + " to " + args[1], EType.Informtion);
+            return true;
+        }
+    }
+}
---
+++ SimplePkgMgr/InstallScriptRuntime/IntegratedFunctions/CmdMkDir.cs
@@ -0,0 +1,31 @@
+﻿using SimpleLogs4Net;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SimplePkgMgr.InstallScriptRuntime.IntegratedFunctions
+{
+    public class CmdMkDir : Cmd
+    {
+        public CmdMkDir(string name) : base(name) { }
+        public override bool Execute(string[] args)
+        {
+            Log.Write("Making directory " + args[0]);
+            try
+            {
+                Directory.CreateDirectory(args[0]);
+            }
+            catch
+            {
+                Log.Write("Making directory Failed " + args[0], EType.Error);
+                return false;
+            }
+            Log.Write("Made directory " + args[0] + " to " + args[1], EType.Informtion);
+            return true;
+        }
+    }
+}
